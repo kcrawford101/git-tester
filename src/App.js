9d09c0cf8edc6fe7c32f6f9c3dd076655b6fc5c7
Kevin Crawford <crawfordkevin101@gmail.com>
2021-11-24 15:44:44 -0500

    first init

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>This is a test so I can deploy this to github pages</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
